0bad7dfca8c6282d27531f4683ff440609b93026
santhosh <santhoshkmr.sdr@gmail.com>
2026-02-23 08:47:11 +0530

    separated fields from DTOS

--- src/main/java/com/sandycode/springbootlearning/Entity/Employee.java
+++ src/main/java/com/sandycode/springbootlearning/Entity/Employee.java
@@ -1,10 +1,6 @@
 package com.sandycode.springbootlearning.Entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
+import jakarta.persistence.*;
 
 import java.util.Objects;
 
@@ -14,15 +10,18 @@ public class Employee {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
-    private String role;
+    private String email;
+    private String DOB;
+    private String Password;
 
-    public Employee() {
-    }
+    public Employee() {}
 
-    public Employee(Integer id, String name, String role) {
+    public Employee(Integer id, String name,String email, String DOB, String Password) {
         this.id = id;
         this.name = name;
-        this.role = role;
+        this.email = email;
+        this.DOB = DOB;
+        this.Password = Password;
     }
 
     public Integer getId() {
@@ -41,28 +40,50 @@ public class Employee {
         this.name = name;
     }
 
-    public String getRole() {
-        return role;
+    public String getEmail() {
+        return email;
     }
 
-    public void setRole(String role) {
-        this.role = role;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
-    @Override
-    public String toString() {
-        return "Employee{" + "id=" + id + ", name='" + name + '\'' + ", role='" + role + '\'' + '}';
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
     }
 
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         Employee employee = (Employee) o;
-        return Objects.equals(id, employee.id) && Objects.equals(name, employee.name) && Objects.equals(role, employee.role);
+        return Objects.equals(id, employee.id) && Objects.equals(name, employee.name) && Objects.equals(email, employee.email) && Objects.equals(DOB, employee.DOB) && Objects.equals(Password, employee.Password);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, role);
+        return Objects.hash(id, name, email, DOB, Password);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", DOB='" + DOB + '\'' +
+                ", Password='" + Password + '\'' +
+                '}';
     }
 }
