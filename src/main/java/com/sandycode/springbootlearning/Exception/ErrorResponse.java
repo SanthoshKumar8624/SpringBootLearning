5df919b9963686d3cee7a29efa5c9bd0956d0682
santhosh <santhoshkmr.sdr@gmail.com>
2026-02-23 08:48:34 +0530

    added ErrorDetails fields

---
+++ src/main/java/com/sandycode/springbootlearning/Exception/ErrorResponse.java
@@ -0,0 +1,40 @@
+package com.sandycode.springbootlearning.Exception;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+public class ErrorResponse {
+    private String message;
+    private int status;
+    private LocalDateTime timestamp;
+    private Map<String, String> errorDetails;
+
+    ErrorResponse(String message, int status) {
+        this.message = message;
+        this.status = status;
+        this.timestamp = LocalDateTime.now();
+    }
+
+    ErrorResponse(String message, int status, Map<String, String> errorDetails) {
+        this.message = message;
+        this.status = status;
+        this.timestamp = LocalDateTime.now();
+        this.errorDetails = errorDetails;
+    }
+
+    public Map<String, String> getErrorDetails() {
+        return errorDetails;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+}
